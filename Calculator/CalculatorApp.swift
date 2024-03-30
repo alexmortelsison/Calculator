24cb650b2ffe68a7f6390d6e5e1e61c6d0a7d12b
alexmortelsison <alexmortelsison@gmail.com>
2024-03-30 13:29:42 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alex Sison on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
